c05c2e6e0294a16c41b6c637aa9282c0abe1063c
azucenafbernal <azucenafbernal21@gmail.com>
2025-10-03 16:40:54 +0200

    Sample Controller

---
+++ src/main/java/com/mantecasl/accommodationapp/AccommodationappApplication.java
@@ -0,0 +1,13 @@
+package com.mantecasl.accommodationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationappApplication.class, args);
+	}
+
+}
