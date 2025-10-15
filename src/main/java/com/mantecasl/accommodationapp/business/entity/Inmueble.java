4702c4db83fcc01901e61ada02ff5bc7f5569023
ZairaMunoz <Zaira.Munoz@alu.uclm.es>
2025-10-15 09:52:11 +0200

    Creación de las entidades necesarias para la base de datos

--- src/main/java/com/mantecasl/accommodationapp/business/entity/Inmueble.java
+++ src/main/java/com/mantecasl/accommodationapp/business/entity/Inmueble.java
@@ -1,7 +1,30 @@
 package com.mantecasl.accommodationapp.business.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Inmueble {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String direccion;
     private double precioNoche;
     
+    public Long getId() {
+        return id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public double getPrecioNoche() {
+        return precioNoche;
+    }
+    public void setPrecioNoche(double precioNoche) {
+        this.precioNoche = precioNoche;
+    }
 }
