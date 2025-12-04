58a86bb6435cad7798eb52c98d78f9e39cbafd36
azucenafbernal <azucenafbernal21@gmail.com>
2025-12-04 19:15:49 +0100

    ultimos retoques

--- src/main/java/com/mantecasl/accommodationapp/business/persistance/ReservaDAO.java
+++ src/main/java/com/mantecasl/accommodationapp/business/persistance/ReservaDAO.java
@@ -1,13 +1,15 @@
 package com.mantecasl.accommodationapp.business.persistance;
 
-import com.mantecasl.accommodationapp.business.entity.Reserva;
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
-import java.util.List;
+import com.mantecasl.accommodationapp.business.entity.Reserva;
 
 @Repository
 public interface ReservaDAO extends JpaRepository<Reserva, Long> {
@@ -45,4 +47,8 @@ public interface ReservaDAO extends JpaRepository<Reserva, Long> {
            "AND r.estado = 'CONFIRMADA'")
     List<Reserva> findReservasConfirmadasFuturasDesdeFecha(@Param("inmuebleId") Long inmuebleId,
                                                            @Param("hoy") Date hoy);
+    
+    @Modifying
+    @Query("DELETE FROM Reserva r WHERE r.inmueble.id = :inmuebleId")
+    void deleteByInmuebleId(@Param("inmuebleId") Long inmuebleId);
 }
